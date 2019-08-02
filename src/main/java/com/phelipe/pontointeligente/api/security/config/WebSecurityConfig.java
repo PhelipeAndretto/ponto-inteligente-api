a4ce950120cce480681a3e7224b42fd6bf71e100
Phelipe José Andretto Pereira <phelipe_andretto@hotmail.com>
2019-08-02 16:44:33 -0300

    Testes com Postman

---
+++ src/main/java/com/phelipe/pontointeligente/api/security/config/WebSecurityConfig.java
@@ -0,0 +1,30 @@
+package com.phelipe.pontointeligente.api.security.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@EnableWebSecurity
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+    	 http.csrf().disable();
+    }
+}
+
+//http
+//.authorizeRequests()
+//    .anyRequest().authenticated()
+//    .and()
+//.httpBasic();
